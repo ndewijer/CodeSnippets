a0ff4be1ebf9cded3fab8ebadebde7e4e1c2960e
ndewijer <nick@dewijer.nl>
2019-07-03 14:04:27 +0200

    expand autoincrease

--- PowerShell/autoIncreasePartition.ps1
+++ PowerShell/autoIncreasePartition.ps1
@@ -1,3 +1,13 @@
 Update-HostStorageCache
-$MaxSize = (Get-PartitionSupportedSize -DriveLetter e).sizeMax
-Resize-Partition -DriveLetter e -Size $MaxSize
+
+sleep 10
+
+get-volume | ForEach-Object { 
+
+    $max_partition = Get-PartitionSupportedSize -DriveLetter $_.DriveLetter
+    $current_partition = Get-Partition -DriveLetter $_.DriveLetter
+
+    if ( ($max_partition.SizeMax) / ($current_partition.Size) -gt 1.1) {
+        Resize-Partition -DriveLetter $_.DriveLetter -Size $max_partition.SizeMax
+    }
+}

c42944b526053dfd53418c4e9563f259a8998cbc
ndewijer <nick@dewijer.nl>
2020-01-02 14:23:38 +0100

    pagefile move

--- PowerShell/randomCommands.ps1
+++ PowerShell/randomCommands.ps1
@@ -24,3 +24,14 @@ get-volume | ForEach-Object {
         Resize-Partition -DriveLetter $_.DriveLetter -Size $max_partition.SizeMax
     }
 }
+
+###
+
+ 
+$computer = Get-WmiObject Win32_computersystem -EnableAllPrivileges;
+$computer.AutomaticManagedPagefile = $false;
+$computer.Put();
+$CurrentPageFile = Get-WmiObject -Query "select * from Win32_PageFileSetting where name='c:\\pagefile.sys'";
+$CurrentPageFile.delete();
+Set-WMIInstance -Class Win32_PageFileSetting -Arguments @{name = "d:\pagefile.sys"; InitialSize = 0; MaximumSize = 0 };
+ 
